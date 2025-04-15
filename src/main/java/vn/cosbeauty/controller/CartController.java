9700bdf71d58e2e0c360d4082ab4c5586796b67a
Ngoc Han <papyconheook@gmail.com>
2025-04-15 21:56:26 +0700

    home - detail product

--- src/main/java/vn/cosbeauty/controller/CartController.java
+++ src/main/java/vn/cosbeauty/controller/CartController.java
@@ -3,8 +3,10 @@ package vn.cosbeauty.controller;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import vn.cosbeauty.DTO.ProductDTO;
+import vn.cosbeauty.entity.CartID;
 import vn.cosbeauty.entity.CartItem;
 import vn.cosbeauty.entity.Customer;
 import vn.cosbeauty.entity.Product;
@@ -12,32 +14,30 @@ import vn.cosbeauty.service.CartService;
 import vn.cosbeauty.service.ProductService;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
-@RequestMapping("api/cart")
+@RequestMapping("/api/cart")
 public class CartController {
 
     @Autowired
     private CartService cartService;
     @Autowired
     private ProductService productService;
+
     @GetMapping("/search")
     public List<Product> searchProducts(@RequestParam String keyword) {
         System.out.println("Tìm kiếm với từ khóa: " + keyword);
         return productService.searchProductsContainingIgnoreCase(keyword);
     }
-    @PostMapping("/cart/add")
-    public String addToCart(@RequestParam("productId") Long productId, HttpSession session) {
-        Customer customer = (Customer) session.getAttribute("customer");
+    @PostMapping("/add")
+    public String addToCart(@RequestParam("customerId") Long customerId,
+                            @RequestParam("productId") Long productId) {
 
-        if (customer == null) {
-            return "redirect:/login";
-        }
+        cartService.addToCart(customerId, productId);
 
-        cartService.addToCart(productId, customer);
-
-        System.out.println("Product ID: " + productId);
-        return "redirect:/shoping-cart";
+        cartService.addToCart(customerId, productId);
+        return String.valueOf(ResponseEntity.ok("Added to cart"));  // Chuyển hướng tới giỏ hàng
     }
 
 
---
+++ src/main/java/vn/cosbeauty/repository/CustomerRepository.java
@@ -0,0 +1,10 @@
+package vn.cosbeauty.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import vn.cosbeauty.entity.Customer;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer,Long> {
+
+}
